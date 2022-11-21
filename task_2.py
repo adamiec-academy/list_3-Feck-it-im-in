6e4e78ff6007817c23a628b05d76f77e3f9d77de
Feck-it-im-in <115720738+Feck-it-im-in@users.noreply.github.com>
2022-11-21 18:28:42 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,11 @@
 def fibonacci_sequence(n):
-    return []
+    result = []
+    if n == 0:
+        return result
+    result.append(0)
+    if n == 1:
+        return result
+    result.append(1)
+    while len(result) != n:
+        result.append(result[-1] + result[-2])
+    return result
